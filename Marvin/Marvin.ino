8acd91bed90e15fd6fabb3d2a347c67808433d86
Cory <cory.andrew.1988@gmail.com>
2015-03-24 22:50:43 -0400

    Minor Marvin.ino cleanup.

--- Marvin/Marvin.ino
+++ Marvin/Marvin.ino
@@ -11,10 +11,13 @@ Point destination(23, 46);
 
 void setup() {
   robot.startPosition();
-  while (robot.center.x != destination.x && robot.center.y != destination.y){
+
+  while (robot.getCenter().calculateDistance(destination) > 0.5) {
     updateWorldObstacles();
-    robot.moveTo(world.nextStepNavigate(robot.getCenter(), destination));
+    Point nextNavPoint = world.nextStepNavigate(robot.getCenter(), destination);
+    robot.moveTo(nextNavPoint);
   }
+
   /*  //Test turning
   robot.turn(M_PI_2);
   delay(1000);
@@ -23,8 +26,6 @@ void setup() {
   //Test Forward Speed
   robot.moveForward(5);
   
-  destination.x = 23;
-  destination.y = 46;
   Serial.begin(9600);
   double distance;
   robot.startPosition();
@@ -37,17 +38,15 @@ void setup() {
   // go forward until obstacle
   distance = robot.frontSensor->getFreeDistance() - 3;
   robot.moveForward(distance);
-  */
+  /**/
   //reached destination tone
-  tone(5, 3000, 1000);                     
-  delay(1000);                               
-
+  tone(5, 3000, 1000);
+  delay(1000);
 }
 
-void loop() {
+void loop() { }
 
-}
-void updateWorldObstacles(){
+void updateWorldObstacles() {
   world.registerObstacle(robot.frontSensor->getObstacle());
   world.registerObstacle(robot.frontRightSensor->getObstacle());
   world.registerObstacle(robot.frontLeftSensor->getObstacle());
